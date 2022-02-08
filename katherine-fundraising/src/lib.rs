b3c370489c26129ab22ec66b52420f79cb078d25
José María Sosa Resendiz <jose.skedar@gmail.com>
2022-02-07 18:34:44 -0600

    moving forward with mini-contract

--- katherine-fundraising/src/lib.rs
+++ katherine-fundraising/src/lib.rs
@@ -30,18 +30,31 @@ pub struct KatherineFundraising {
 impl KatherineFundraising {
     #[init]
     pub fn new(owner_id: AccountId, staking_goal: u128) -> Self {
-        assert!(!env::state_exists(), "The contract is already initialized");
+        // assert!(!env::state_exists(), "The contract is already initialized");
         Self {
             owner_id: owner_id,
             accounts: UnorderedMap::new(b"A".to_vec()),
             total_available: 0,
             staking_goal: staking_goal,
-            min_deposit_amount: 10 * NEAR,
+            min_deposit_amount: 1 * NEAR,
         }
     }
 
     #[payable]
     pub fn deposit_and_stake(&mut self) {
         self.internal_deposit();
+    }
+
+    /*****************************/
+    /* staking-pool View methods */
+    /*****************************/
+
+    pub fn get_account_available_balance(&self, account_id: AccountId) -> U128String {
+        let acc = self.internal_get_account(&account_id);
+        acc.available.into()
+    }
+
+    pub fn get_contract_total_available(&self) -> U128String {
+        self.total_available.into()
     }
 }
--- katherine-fundraising/src/types.rs
+++ katherine-fundraising/src/types.rs
@@ -1,1 +1,6 @@
+use near_sdk::json_types::{U128};
+
 pub const NEAR: u128 = 1_000_000_000_000_000_000_000_000;
+
+/// Balance wrapped into a struct for JSON serialization as a string.
+pub type U128String = U128;
