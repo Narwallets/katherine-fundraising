83fe6e15d3ea12bef3448f32522c343074bab986
Jose Maria Sosa <39598219+josemariasosa@users.noreply.github.com>
2022-02-14 20:10:24 -0600

    Katherine moves funds back to the users or to the Meta Pool contract. (#2)
    * Return funds to users
    
    * Katherine moves funds back to the users or to metapool
    
    * Only the owner can call
    
    * Updating docs.
    
    * Removing wasm file
    
    * Contract Logic diagram

--- README.md
+++ README.md
@@ -2,13 +2,62 @@
 
 Allow any project to bootstrap liquidity through staking on Meta Pool.
 
+Contract Logic:
+
+![Katherine Contract Logic](media/logic.png)
+
 ## Build the contract
+
+Run the `build.sh` script.
+
+```sh
+./build.sh
+```
+
+## Deploy the contract in Testnet
+
+https://docs.near.org/docs/tools/near-cli#near-deploy
+
+```sh
+export NEAR_ENV=testnet
+
+rm -rf neardev/ && near dev-deploy --wasmFile res/katherine_fundraising.wasm --initFunction new --initArgs '{"owner_id": "jomsox.testnet", "staking_goal": 10}' && export $(grep -v '^#' neardev/dev-account.env | xargs)
+```
+
+A new account will be created for the contract. Note how the last command exported CONTRACT_NAME.
+
+## Deposit to the contract
+
+https://docs.near.org/docs/tools/near-cli#near-call
+
+Deposit to the testnet contract using a test multiple test accounts:
+
+```sh
+near call $CONTRACT_NAME deposit_and_stake --accountId jomsox.testnet --deposit 2
+near call $CONTRACT_NAME deposit_and_stake --accountId huxley.testnet --deposit 11 
+```
+
+## View the contract total available amount
+
+```sh
+near view $CONTRACT_NAME get_contract_total_available '{}'
+```
+
+
+near call $CONTRACT_NAME evaluate_at_due --accountId huxley.testnet 
+
+
+## Deploy local Node
+
+The `local_near` command is part of the Kurtosis development environment: https://docs.near.org/docs/tools/kurtosis-localnet.
+
+1. Build the contract
 
 ```sh
 RUSTFLAGS='-C link-arg=-s' cargo +stable build --all --target wasm32-unknown-unknown --release
 ```
 
-## Deploy the contract in a local node
+2. Deploy the contract in a local node
 
 https://docs.near.org/docs/tools/near-cli#near-deploy
 
@@ -18,7 +67,7 @@ cd katherine-fundraising
 local_near deploy --accountId jomsox.test.near --wasmFile target/wasm32-unknown-unknown/release/katherine_fundraising.wasm --initFunction new --initArgs '{"owner_id": "jomsox.test.near", "staking_goal": 10000}'
 ```
 
-## Deposit to the contract
+3. Deposit to the contract
 
 https://docs.near.org/docs/tools/near-cli#near-call
 
--- katherine-fundraising/src/internal.rs
+++ katherine-fundraising/src/internal.rs
@@ -1,5 +1,7 @@
 use crate::*;
 use near_sdk::{log, AccountId};
+use near_sdk::Promise;
+use near_sdk::serde_json::{json};
 
 pub use crate::types::*;
 
@@ -24,7 +26,6 @@ impl KatherineFundraising {
 
     pub(crate) fn internal_deposit_attached_near_into(&mut self, account_id: AccountId) {
         let amount = env::attached_deposit();
-
         let mut account = self.internal_get_account(&account_id);
 
         account.available += amount;
@@ -41,17 +42,35 @@ impl KatherineFundraising {
     }
 
     /// Inner method to get the given account or a new default value account.
-    pub(crate) fn internal_get_account(&self, account_id: &String) -> Account {
+    pub(crate) fn internal_get_account(&self, account_id: &AccountId) -> Account {
         self.accounts.get(account_id).unwrap_or_default()
     }
 
     /// Inner method to save the given account for a given account ID.
     /// If the account balances are 0, the account is deleted instead to release storage.
-    pub(crate) fn internal_update_account(&mut self, account_id: &String, account: &Account) {
+    pub(crate) fn internal_update_account(&mut self, account_id: &AccountId, account: &Account) {
         if account.is_empty() {
             self.accounts.remove(account_id);
         } else {
             self.accounts.insert(account_id, &account); //insert_or_update
         }
+    }
+
+    pub(crate) fn transfer_back_to_account(&mut self, account_id: &AccountId, account: &mut Account) {
+        let available: Balance = account.available;
+        Promise::new(account_id.to_string()).transfer(available);
+        account.available -= available;
+        self.total_available -= available;
+        self.internal_update_account(&account_id, &account);
+    }
+
+    pub(crate) fn internal_stake_funds(&mut self) {
+        let metapool: AccountId = String::from("meta-v2.pool.testnet");
+        Promise::new(metapool.to_string()).function_call(
+            b"deposit_and_stake".to_vec(),
+            json!({}).to_string().as_bytes().to_vec(),
+            self.total_available,
+            GAS,
+        );
     }
 }
