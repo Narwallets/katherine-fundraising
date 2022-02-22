f3e84cb65554aaeda1ea970c2aeeeb12d9d69c4c
Iván Chavero <ivan.chavero@ugd.gob.mx>
2022-02-22 01:44:10 -0700

    Add kickstarter CRUD operations

--- katherine-fundraising/src/lib.rs
+++ katherine-fundraising/src/lib.rs
@@ -1,9 +1,18 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{UnorderedMap};
-use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault, Balance, Gas};
+use near_sdk::collections::{UnorderedMap, Vector};
+use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault, Balance, Gas, Timestamp};
 
 pub mod account;
 pub use crate::account::*;
+
+pub mod kickstarter;
+pub use crate::kickstarter::*;
+
+pub mod funder;
+pub use crate::funder::*;
+
+pub mod goal;
+pub use crate::goal::*;
 
 pub mod types;
 pub use crate::types::*;
@@ -19,6 +28,10 @@ pub struct KatherineFundraising {
     pub owner_id: AccountId,
 
     pub accounts: UnorderedMap<AccountId, Account>,
+
+    /// Kickstarter list
+    //pub kickstarters: UnorderedMap<u32, Kickstarter>,
+    pub kickstarters: Vector<Kickstarter>,
 
     pub total_available: Balance,
 
@@ -36,6 +49,7 @@ impl KatherineFundraising {
         Self {
             owner_id,
             accounts: UnorderedMap::new(b"A".to_vec()),
+            kickstarters: Vector::new(b"Kickstarters".to_vec()),
             total_available: 0,
             staking_goal: staking_goal * NEAR,
             min_deposit_amount: 1 * NEAR,
@@ -71,4 +85,82 @@ impl KatherineFundraising {
     pub fn get_contract_total_available(&self) -> U128String {
         self.total_available.into()
     }
+
+    /*****************************/
+    /*   Kickstarter functions   */
+    /*****************************/
+
+    /// Creates a new kickstarter entry in persistent storage
+    pub fn create_kickstarter(&mut self, 
+        name: String,
+        slug: String,
+        finish_timestamp: Timestamp,
+        open_timestamp: Timestamp,
+        close_timestamp: Timestamp,
+        vesting_timestamp: Timestamp,
+        cliff_timestamp: Timestamp) {
+
+        let k = Kickstarter {
+            id: self.kickstarters.len(),
+            name: name,
+            slug: slug,
+            goals: Vec::new(),
+            funders: Vec::new(),
+            owner: env::predecessor_account_id(),
+            active: true,
+            succesful: false,
+            creation_timestamp: env::block_timestamp(),
+            //TODO: get this from arguments
+            finish_timestamp: env::block_timestamp(),
+            open_timestamp: env::block_timestamp(),
+            close_timestamp: env::block_timestamp(),
+            vesting_timestamp: env::block_timestamp(),
+            cliff_timestamp: env::block_timestamp(),
+        };
+
+        self.kickstarters.push(&k);
+    }
+
+    /// Returns a list of the kickstarter entries
+    pub fn list_kickstarters(&self) -> Vec<Kickstarter> {
+        self.kickstarters.to_vec()
+    }
+
+
+    pub fn delete_kickstarter(&mut self, id: u64) {
+        self.kickstarters.swap_remove(id);
+    }
+
+    /// Update a kickstarter
+    pub fn update_kickstarter(&mut self, 
+        id: u64,
+        name: String,
+        slug: String,
+        finish_timestamp: Timestamp,
+        open_timestamp: Timestamp,
+        close_timestamp: Timestamp,
+        vesting_timestamp: Timestamp,
+        cliff_timestamp: Timestamp) {
+
+        let k = Kickstarter {
+            id: id,
+            name: name,
+            slug: slug,
+            goals: Vec::new(),
+            funders: Vec::new(),
+            owner: env::predecessor_account_id(),
+            active: true,
+            succesful: false,
+            creation_timestamp: env::block_timestamp(),
+            //TODO: get this from arguments
+            finish_timestamp: env::block_timestamp(),
+            open_timestamp: env::block_timestamp(),
+            close_timestamp: env::block_timestamp(),
+            vesting_timestamp: env::block_timestamp(),
+            cliff_timestamp: env::block_timestamp(),
+        };
+
+        self.kickstarters.replace(id, &k);
+    }
+
 }
