f195e507cdc43ede4fa53d898b8ba181ff50d655
José María Sosa Resendiz <jose.skedar@gmail.com>
2022-02-24 18:20:00 -0600

    Defining the reward token IOU based on the Goal parameters 🦾

--- katherine-fundraising/src/iou_note.rs
+++ katherine-fundraising/src/iou_note.rs
@@ -13,11 +13,20 @@ pub enum IOUNoteDenomination {
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Debug, PartialEq, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub struct IOUNote {
+    /// Incremental Folio
     pub id: IOUNoteId,
+
+    /// Total amount of the denominated token
     pub amount: Balance,
     pub denomination: IOUNoteDenomination,
+
     pub supporter_id: AccountId,
     pub kickstarter_id: KickstarterId,
+
+    /// Date when the token release starts
     pub cliff_timestamp: Timestamp,
+
+    /// The period from cliff to end timestamp is when the user could withdraw tokens
+    /// following a linear release
     pub end_timestamp: Timestamp,
 }
