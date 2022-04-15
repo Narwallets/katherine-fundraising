22ab114cec6d22b5e084538891d889f1ac639fa6
LautaroC <lautarocutri@gmail.com>
2022-04-15 16:45:47 -0300

    get_kickstarters_to_unfreeze

--- katherine-fundraising-contract/src/lib.rs
+++ katherine-fundraising-contract/src/lib.rs
@@ -123,6 +123,28 @@ impl KatherineFundraising {
         }
     }
 
+    /// Returns kickstarters ids ready to unfreeze.
+    pub fn get_kickstarters_two_unfreeze(
+        &self,
+        from_index: KickstarterIdJSON,
+        limit: KickstarterIdJSON,
+    ) -> Option<Vec<KickstarterIdJSON>> {
+        let kickstarters_len = self.kickstarters.len();
+        let start: u64 = from_index.into();
+        if start >= kickstarters_len {
+            return None;
+        }
+        let mut result: Vec<KickstarterIdJSON> = Vec::new();
+        for index in start..std::cmp::min(start + limit as u64, kickstarters_len) {
+            let kickstarter = self.internal_get_kickstarter(index as u32);
+            if kickstarter.successful == Some(true) && kickstarter.stnear_price_at_unfreeze == None {
+                kickstarter.assert_funds_can_be_unfreezed();
+                result.push(KickstarterIdJSON::from(kickstarter.id));
+            }
+        }
+        Some(result)
+    }
+
     /// Start the cross-contract call to unfreeze the kickstarter funds.
     pub fn unfreeze_kickstarter_funds(&mut self, kickstarter_id: KickstarterIdJSON) {
         let kickstarter = self.internal_get_kickstarter(kickstarter_id);
